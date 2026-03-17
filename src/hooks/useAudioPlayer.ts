f7344fcd00019a00db8bb5a7c3044e3d4ddf1088
Alessio Frittoli <info@alessiofrittoli.it>
2026-03-17 14:44:39 +0100

    feat: add `useAudioPlayer` hook

---
+++ src/hooks/useAudioPlayer.ts
@@ -0,0 +1,21 @@
+import { useRef } from 'react'
+import type { Queue } from '@/types'
+import { useMediaPlayer, type UseMediaPlayer, type UseMediaPlayerOptions } from '@/hooks/useMediaPlayer'
+
+
+export type UseAudioPlayerOptions<T extends Queue = Queue> = Omit<UseMediaPlayerOptions<T>, 'media'>
+
+
+/**
+ * Easily handle React audio players.
+ * 
+ * @param options An object defining required data. See {@link UseAudioPlayerOptions} for more info.
+ * @returns An object defining media player state and utilities. See {@link UseMediaPlayer} for more info.
+ */
+export const useAudioPlayer = <T extends Queue = Queue>( options: UseAudioPlayerOptions<T> ): UseMediaPlayer<T> => (
+	useMediaPlayer<T>( {
+		// eslint-disable-next-line react-hooks/refs
+		media: useRef( typeof window !== 'undefined' ? new Audio() : undefined ).current,
+		...options
+	} )
+)
